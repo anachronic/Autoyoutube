43a7c4587098fd61cb0a43bb52de70c50019a3ef
Nicolas Salas V <nikosalas@gmail.com>
2015-08-23 03:01:21 -0300

    Finish basic Song class which can download a Song and put tags on it.

--- src/main/java/org/anachronic/autoyoutube/App.java
+++ src/main/java/org/anachronic/autoyoutube/App.java
@@ -3,6 +3,8 @@ package org.anachronic.autoyoutube;
 
 public class App 
 {
+    private static final String tempDir = ".temp";
+
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
--- src/main/java/org/anachronic/autoyoutube/Song.java
+++ src/main/java/org/anachronic/autoyoutube/Song.java
@@ -1,33 +1,60 @@
 package org.anachronic.autoyoutube;
 
 
+import com.mpatric.mp3agic.*;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.UUID;
 
 public class Song {
 
     private String url;
     private String command;
+    private String uuid;
 
-    public Song(String url){
+    public Song(String url) {
         this.url = url;
 
-        this.command = "youtube-dl -x " + url + " --audio-format mp3";
+        this.uuid = UUID.randomUUID().toString() + ".mp3";
+        this.command = "youtube-dl -x " + url + " --audio-format mp3 -o " + this.uuid;
     }
 
-    public String getTitle() throws IOException{
+    private static void reportIOEx() {
+        System.err.println("Can't read/write process output. Check that you have ffmpeg and youtube-dl installed");
+        System.err.println("Don't forget to check that you can actually run those processes.");
+        System.err.println("Exiting...");
+    }
+
+    public String getTitle() {
         Runtime rt = Runtime.getRuntime();
         String thecommand = this.command + " -e";
-        Process proc = rt.exec(thecommand);
+        Process proc = null;
+        try {
+            proc = rt.exec(thecommand);
+        } catch (IOException e) {
+            reportIOEx();
+        }
 
         BufferedReader stdInput = new BufferedReader(new
                 InputStreamReader(proc.getInputStream()));
 
         String s;
         String result = null;
-        while ((s = stdInput.readLine()) != null) {
-            result=s;
+        try {
+            while ((s = stdInput.readLine()) != null) {
+                result = s;
+            }
+        } catch (IOException e) {
+            reportIOEx();
+        }
+
+        try {
+            proc.waitFor();
+        } catch (InterruptedException e) {
+            System.err.println("ERROR: Couldn't get YouTube title from video. Process was aborted. Exiting...");
+            System.exit(-1);
         }
 
         return result;
@@ -39,24 +66,21 @@ public class Song {
 
     public void setUrl(String url) {
         this.url = url;
-        this.command = "youtube-dl -x " + url + " --audio-format mp3";
     }
 
-    public boolean download(){
+    private boolean mp3Download() {
         Runtime r = Runtime.getRuntime();
         Process proc = null;
         try {
             proc = r.exec(this.command);
         } catch (IOException e) {
-            System.err.println("Cant read/write process output. Check that you have ffmpeg and youtube-dl installed");
-            System.err.println("Don't forget to check that you can actually run those processes.");
-            System.err.println("Exiting...");
+
             return false;
         }
 
         try {
             int result = proc.waitFor();
-            if(result != 0){
+            if (result != 0) {
                 System.err.println("WARNING: Downloading process exited with non-zero exitcode" + result);
             }
         } catch (InterruptedException e) {
@@ -65,5 +89,56 @@ public class Song {
         }
 
         return true;
+    }
+
+    public void downloadWithTags(boolean artistExpectedAtBeginning) {
+        downloadWithTags(artistExpectedAtBeginning, " - ");
+    }
+    public void downloadWithTags(boolean artistExpectedAtBeginning, String separator) {
+        String title = getTitle();
+
+        mp3Download();
+
+        Mp3File thisFile = null;
+        try {
+            thisFile = new Mp3File(this.uuid);
+        } catch (Exception e){
+            System.err.println("Can't seem to open and/or read the downloaded file. Does it exist?");
+            System.err.println("Is it an mp3 file?");
+            System.exit(0);
+        }
+
+        if (thisFile.hasId3v1Tag()) {
+            thisFile.removeId3v1Tag();
+        }
+        if (thisFile.hasId3v2Tag()) {
+            thisFile.removeId3v2Tag();
+        }
+        if (thisFile.hasCustomTag()) {
+            thisFile.removeCustomTag();
+        }
+
+        ID3v2 tag = new ID3v24Tag();
+
+        String[] split = title.split(separator);
+        String artist, songName;
+        if(artistExpectedAtBeginning){
+            artist = split[0];
+            songName = split[1];
+        } else {
+            artist = split[1];
+            songName = split[0];
+        }
+
+        tag.setArtist(artist);
+        tag.setTitle(songName);
+
+        thisFile.setId3v2Tag(tag);
+
+        try {
+            thisFile.save(artist + " - " + songName + ".mp3");
+        } catch (Exception e){
+            System.err.println("Can't save the new downloaded file. Do you have write permissions on this folder?");
+        }
     }
 }
