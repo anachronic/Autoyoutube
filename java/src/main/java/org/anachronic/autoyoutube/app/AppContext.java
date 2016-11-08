b307bada233758608e10b6cd46a2d4d8439ddc7b
Nicolas Salas V <nikosalas@gmail.com>
2016-11-08 03:18:27 -0300

    Get rid of Java code

--- java/src/main/java/org/anachronic/autoyoutube/app/AppContext.java
+++
@@ -1,9 +0,0 @@
-package org.anachronic.autoyoutube.app;
-
-
-public class AppContext {
-
-    public static final String TEMP_DIR = ".temp";
-    public static final String APP_NAME = "Autoyoutube";
-    public static final String DEFAULT_SEPARATOR = " - ";
-}
