a46817aca7ca8b255fa0d739fc74dd3569ced99c
Nicolás Salas V <nikosalas@gmail.com>
2016-11-08 16:28:40 -0300

    Display search title result

--- ui/aytwindow.py
+++ ui/aytwindow.py
@@ -2,14 +2,17 @@ import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
+from songs import Song
+
 
 class AytWindow(Gtk.Window):
     def __init__(self):
         Gtk.Window.__init__(self, title="Autoyoutube")
         self.set_border_width(10)
 
+        self.set_default_size(800, 100)
+
         hbox = Gtk.Box(spacing=6)
-        self.add(hbox)
 
         self.url = Gtk.Entry()
         hbox.pack_start(self.url, True, True, 0)
@@ -18,5 +21,15 @@ class AytWindow(Gtk.Window):
         button.connect("clicked", self.on_search)
         hbox.pack_start(button, True, True, 0)
 
+        self.result_label = Gtk.Label()
+
+        self.vbox = Gtk.Box(spacing=6, orientation=Gtk.Orientation.VERTICAL)
+        self.vbox.pack_start(hbox, True, True, 0)
+        self.vbox.pack_start(self.result_label, True, True, 0)
+
+        self.add(self.vbox)
+
     def on_search(self, widget):
-        print("Search bro")
+        song = Song(self.url.get_text())
+
+        self.result_label.set_text(song.get_title())
