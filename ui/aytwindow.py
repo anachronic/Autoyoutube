a903bac9eda2cc28524be422ef47ba15a0e86fb4
Nicolas Salas V <nikosalas@gmail.com>
2016-11-16 21:20:08 -0300

    Add TreeView to the UI with results displayable for songs

--- ui/aytwindow.py
+++ ui/aytwindow.py
@@ -27,9 +27,56 @@ class AytWindow(Gtk.Window):
         self.vbox.pack_start(hbox, True, True, 0)
         self.vbox.pack_start(self.result_label, True, True, 0)
 
+        # we begin with no candidates whatsoever
+        self.candidates = []
+
+        columns = ("Artist", "Song")
+
+        self.list_store = Gtk.ListStore(str, str)
+        self.treeview = Gtk.TreeView(self.list_store)
+
+        for i, column in enumerate(columns):
+            renderer = Gtk.CellRendererText()
+            col = Gtk.TreeViewColumn(column, renderer, text=i)
+            self.treeview.append_column(col)
+
+        self.vbox.pack_start(self.treeview, True, True, 0)
+
         self.add(self.vbox)
 
     def on_search(self, widget):
-        song = Song(self.url.get_text())
+        url = self.url.get_text()
 
-        self.result_label.set_text(song.get_title())
+        try:
+            is_playlist = self.url_is_playlist(url)
+        except ValueError:
+            self.result_label.set_text("That is absolutely not an URL.")
+            return
+
+        songs = []
+        if not is_playlist:
+            song = Song(url)
+            song.fill_song_metadata()
+            # Append song's artist and title into self.candidates
+            # then add the items to the list_store. that should suffice
+
+            row = (song.get_artist(), song.get_name())
+            self.list_store.append(row)
+            self.result_label.set_text(song.get_title())
+
+
+    def url_is_playlist(self, url):
+        # For now we do this here
+        # TODO: Need to move this somewhere else
+        playlist_words = ("list=", "playlist?")
+        assert_words = ("youtube.com")
+
+        for word in assert_words:
+            if url.find(word) == -1:
+                raise ValueError
+
+        for word in playlist_words:
+            if url.find(word) >= 0:
+                return True
+
+        return False
